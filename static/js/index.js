f6ea2fa7b8d0cca4d53caaa5e746ad8c5270bd48
thejadeR <1056497498@qq.com>
2019-03-02 11:48:22 +0800

    修改屬性獲取‘

--- static/js/index.js
+++ static/js/index.js
@@ -6,21 +6,14 @@ $('.themusic').click(function () {
     console.log('點擊')
     // <source src="/static/upload/index/daogu.mp3" type="audio/mpeg" class="ms_src">
 
-    $('.ms_title').html(this.firstElementChild.attributes[0].value)
 
+    $('.ms_title').html($(this).find('p').first().attr('data-title'))
 
     $('.the_audio').empty()
-    $('<audio controls><source src="/static/upload/index/' + this.firstElementChild.attributes[1].value + ' " type="audio/mpeg" class="ms_src"> 您的浏览器不支持 audio 元素。</audio>').appendTo('.the_audio')
-
-    // $('.ms_src').attr({'src':'/static/upload/index/'+this.firstElementChild.attributes[1].value})
+    $('<audio controls><source src="/static/upload/index/' + $(this).find('p').first().attr('data-src') + ' " type="audio/mpeg" class="ms_src"> 您的浏览器不支持 audio 元素。</audio>').appendTo('.the_audio')
 
 
-// /static/upload/index/
-//       console.log( $('.ms_src').attr({'src':this.firstElementChild.attributes[1].value}) )
 })
-
-
-
 
 
 
